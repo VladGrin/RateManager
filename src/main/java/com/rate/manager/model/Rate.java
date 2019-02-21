1f93b0ef8a15b29aa56e58cfc65fa11f596a5487
Volodymyr <vlad5855051@gmail.com>
2019-02-21 14:20:43 +0200

    restart

--- src/main/java/com/rate/manager/model/Rate.java
+++ src/main/java/com/rate/manager/model/Rate.java
@@ -3,15 +3,15 @@ package com.rate.manager.model;
 import lombok.Builder;
 import lombok.Data;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
 
 @Data
 @Builder
 public class Rate {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String currencyName;
